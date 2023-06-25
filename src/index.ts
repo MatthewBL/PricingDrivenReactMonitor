6c5231a5bbe99335b9861381b88fbfd3ee6b2da6
Alex-GF <yalejandro9@gmail.com>
2023-06-25 12:33:18 +0200

    FEAT: UPDATED README AND BINARY OPERATORS ADDED

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { Feature, On, Default, Loading, ErrorFallback } from "./lib/components/feature/Feature";
 import useGenericFeature from "./lib/components/feature/useGenericFeature";
-import { FeatureContext } from "./lib/components/feature/FeatureContext";
-import FeatureRetriever from "./lib/components/feature/FeatureRetriever";
 import { feature } from "./lib/logic/model/Feature";
+import { and, or, iff, implies } from "./lib/logic/model/BinaryLogicalPredicate";
 
-export { Feature, On, Default, Loading, ErrorFallback, FeatureContext, FeatureRetriever, feature, useGenericFeature };
+export { Feature, On, Default, Loading, ErrorFallback, feature, useGenericFeature, and, or, iff, implies};
