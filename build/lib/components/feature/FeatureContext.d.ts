0ac91d560623738be290839a0b1c4938889d49b1
Alex-GF <yalejandro9@gmail.com>
2023-09-12 16:39:39 +0200

    REFACTOR: BUILD FOLDER DELETED

--- build/lib/components/feature/FeatureContext.d.ts
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import FeatureRetriever from "./FeatureRetriever";
-interface AppContext {
-    featureRetriever: FeatureRetriever;
-}
-export declare const FeatureContext: React.Context<AppContext>;
-export {};
--- build/lib/logic/model/util.d.ts
+++
@@ -1,9 +0,0 @@
-import FeatureRetriever, { AttributeValue } from "../../components/feature/FeatureRetriever";
-import { NAryFunction } from "./NAryFunction";
-export declare function makeFeatureAttributeRetrievers(featureRetriever: FeatureRetriever): {
-    feature: (featureId: string) => NAryFunction<boolean>;
-    attribute: (attributeId: string) => NAryFunction<AttributeValue>;
-};
-export declare function numberFunction<T>(left: NAryFunction<number> | number, right: NAryFunction<number> | number, factory: (left: NAryFunction<number>, right: NAryFunction<number>) => T): T;
-export declare function booleanFunction<T>(left: NAryFunction<boolean> | boolean, right: NAryFunction<boolean> | boolean, factory: (left: NAryFunction<boolean>, right: NAryFunction<boolean>) => T): T;
-export declare function attrValueFunction<T>(left: NAryFunction<AttributeValue> | AttributeValue, right: NAryFunction<AttributeValue> | AttributeValue, factory: (left: NAryFunction<AttributeValue>, right: NAryFunction<AttributeValue>) => T): T;
