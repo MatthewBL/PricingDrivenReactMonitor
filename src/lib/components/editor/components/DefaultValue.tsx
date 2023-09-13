ce6673b98dd88664f4e94ae8e901c1461f583745
Pedro González Marcos <pedgonmar2@alum.us.es>
2023-09-13 11:42:39 +0200

    feat: add editor  specific components

---
+++ src/lib/components/editor/components/DefaultValue.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { AttributeType } from "../types";
+
+interface DefaultValueProps {
+  id: string;
+  name: string;
+  type: AttributeType;
+  value: string;
+  onChange: (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => void;
+}
+
+export function DefaultValue({
+  id,
+  name,
+  type,
+  value,
+  onChange,
+}: DefaultValueProps) {
+  switch (type) {
+    case "TEXT":
+      return (
+        <input
+          id={id}
+          name={name}
+          type="text"
+          onChange={onChange}
+          value={value}
+        />
+      );
+    case "NUMERIC":
+      return (
+        <input
+          id={id}
+          name={name}
+          type="number"
+          onChange={onChange}
+          value={Number(value)}
+        />
+      );
+    case "CONDITION":
+      return (
+        <select id={id} name={name} value={value} onChange={onChange}>
+          <option value="1">YES</option>
+          <option value="0">NO</option>
+        </select>
+      );
+  }
+}
---
+++ src/lib/components/editor/components/NavBar.tsx
@@ -0,0 +1,54 @@
+import React, { useContext, useState } from "react";
+import { NavLink } from "react-router-dom";
+import { Toggle } from "../components/Toggle";
+import { EditorContext } from "../context/EditorContextProvider";
+import { Button } from "../components/Button";
+import { attributesToFeatures } from "../utils";
+import { AttributesContext } from "../context/AttributesProvider";
+import { PricingContext } from "../types";
+
+interface NavBarProps {
+  onSave: (pricingContext: PricingContext) => void;
+}
+
+export function NavBar({ onSave }: NavBarProps) {
+  const attrCtx = useContext(AttributesContext);
+  const { theme, returnTo, pricingContext, setPricingContext } =
+    useContext(EditorContext);
+  const [hidden, setHidden] = useState(false);
+
+  const handleClick = () => {
+    setHidden(!hidden);
+  };
+
+  const features = attributesToFeatures(attrCtx.state.data);
+
+  const updatePricingContext = () => {
+    setPricingContext({ ...pricingContext, features });
+  };
+
+  return (
+    <>
+      <nav className={theme} id="sidebar" hidden={hidden}>
+        <ul>
+          <li>
+            <NavLink to="attributes">Attributes</NavLink>
+          </li>
+          <li>
+            <NavLink to="user-context">User Context</NavLink>
+          </li>
+          <li>
+            <NavLink to="plans">Pricing plans</NavLink>
+          </li>
+          <li>
+            <NavLink to={returnTo}>Return to page</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <Toggle isHidden={hidden} onClick={handleClick} />
+      <Button onClick={updatePricingContext} text="Save" />
+      <Button onClick={() => onSave(pricingContext)} text="Send" />
+      <pre>{JSON.stringify(pricingContext, null, 2)}</pre>
+    </>
+  );
+}
