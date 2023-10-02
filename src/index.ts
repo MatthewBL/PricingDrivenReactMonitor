392915ef4d44b140acc7cc124e437397c3ea5434
Pedro González Marcos <pedgonmar2@alum.us.es>
2023-10-02 11:42:43 +0200

    fix: change import of editor index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import {
   Loading,
   ErrorFallback,
 } from "./lib/components/feature/Feature";
-import { PricingConfigurationRoutes } from "./lib/components/editor";
+export { PricingConfigurationRoutes } from "./lib/components/editor";
 import useGenericFeature from "./lib/components/feature/useGenericFeature";
 import { feature } from "./lib/logic/model/Feature";
 import {
@@ -21,7 +21,6 @@ export {
   Default,
   Loading,
   ErrorFallback,
-  PricingConfigurationRoutes,
   feature,
   useGenericFeature,
   and,
